5f3750d4ed07b7c9bc3dfb5ddec57946a1d7ddbd
buihuukhoi <50270984+buihuukhoi@users.noreply.github.com>
2020-08-18 02:07:19 +0800

    Update DQN.py

--- Training-Local/DQN.py
+++ Training-Local/DQN.py
@@ -16,10 +16,11 @@ class DQN:
             input_shape_1=(21, 9, 2),  # The number of inputs for the DQN network
             input_shape_2=(24,),
             action_space=6,  # The number of actions for the DQN network
-            gamma=0.99,  # The discount factor
+            #gamma=0.99,  # The discount factor
+            gamma=0.9
             epsilon=1,  # Epsilon - the exploration factor
             epsilon_min=0.01,  # The minimum epsilon
-            epsilon_decay=0.9999,  # The decay epsilon for each update_epsilon time
+            epsilon_decay=0.99999,  # The decay epsilon for each update_epsilon time
             learning_rate=0.00025,  # The learning rate for the DQN network
             tau=0.125,  # The factor for updating the DQN target network from the DQN network
             sess=None,
