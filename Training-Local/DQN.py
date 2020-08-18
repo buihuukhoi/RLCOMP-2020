2b6f109d5abbabac61aae299253023c7b29c68c7
buihuukhoi <50270984+buihuukhoi@users.noreply.github.com>
2020-08-18 09:03:54 +0800

    Update DQN.py

--- Training-Local/DQN.py
+++ Training-Local/DQN.py
@@ -20,7 +20,7 @@ class DQN:
             #gamma=0.9
             epsilon=1,  # Epsilon - the exploration factor
             epsilon_min=0.01,  # The minimum epsilon
-            epsilon_decay=0.99999,  # The decay epsilon for each update_epsilon time
+            epsilon_decay=0.9999,  # The decay epsilon for each update_epsilon time
             learning_rate=0.00025,  # The learning rate for the DQN network
             tau=0.125,  # The factor for updating the DQN target network from the DQN network
             sess=None,
